50271866faddf97e3286b7a68bd9133361c10c46
Brant Stuns <brantstuns@gmail.com>
2017-11-29 01:42:59 -0700

    Updated to all latest versions of dependencies including React 16

--- client/js/index.js
+++ client/js/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Router';
 import '../styles/styles.scss';
+import 'raf/polyfill';
 
 ReactDOM.render(<Router/>, document.getElementById('appRender'));
